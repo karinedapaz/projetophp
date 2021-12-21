ed036b599e0b5da42a93854aff353b44df96519a
karinedapaz@gmail.com <samiradaguerra@gmail.com>
2021-12-21 14:33:55 -0300

    olamundo testando

---
+++ olamundo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Primeiro Exempl PHP</title>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php
+         echo "<h2>Ola, <br/> Mundo!</h2>";
+
+     ?>
+    
+</body>
+</html>
