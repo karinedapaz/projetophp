48eb07b7c0fe73a7dc1c21e86ebe448d2bbea767
karinedapaz@gmail.com <samiradaguerra@gmail.com>
2021-12-21 15:19:13 -0300

    olamundo

--- olamundo.php
+++ olamundo.php
@@ -3,6 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Primeiro Exempl PHP</title>
+    <style>
+        h2 {
+           color: #80a2ff;
+           text-shadow: 1px 1px 1px black;
+        }
+    </style>
 </head>
 <body>
     <h1>Testando PHP</h1>
