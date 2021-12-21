e86165cfb7ff8a9d95b9b1d192cf6a5d97bf4bca
karinedapaz@gmail.com <samiradaguerra@gmail.com>
2021-12-21 16:20:50 -0300

    variaveis

--- aula04.php
+++ aula04.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
-<html lang="pt">
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
 <body>
     <?php
-     $n = 4
-     $no = "Gustavo";
-     echo $no;
+     $nome = "Maria";
+     $idade = 18;
+     echo "$nome tem $idade anos!";
     ?>
 </body>
 </html>
