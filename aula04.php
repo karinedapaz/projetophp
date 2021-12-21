5bb7454910c90dae7d5dc38479c3a2294bd70f46
karinedapaz@gmail.com <samiradaguerra@gmail.com>
2021-12-21 15:46:21 -0300

    aula variaveis

---
+++ aula04.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     $n = 4
+     $no = "Gustavo";
+     echo $no;
+    ?>
+</body>
+</html>
