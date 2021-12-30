2018d37d68cd6844b8edacc150af53b3323b59f8
karinedapaz@gmail.com <samiradaguerra@gmail.com>
2021-12-30 10:09:08 -0300

    operadores aritmeticos

---
+++ aula05/somador.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="_css/stilo.css">
+    <title></title>
+</head>
+<body>
+<div>
+    <?php
+       $n1 = 3;
+       $n2 = 2;
+       $s = $n1 + $n2;
+       echo "A soma entre $n1 e $n2 e igual a $s";
+    ?>
+</body>
+</html>
