879a95377d6fbe3592ccd1851592725ad50163bd
karinedapaz@gmail.com <samiradaguerra@gmail.com>
2022-01-03 15:26:03 -0300

    aula09

--- aula09/exercicio01.php
+++ aula09/exercicio01.php
@@ -9,7 +9,17 @@
 <div> 
     <?php
     $a = isset($_GET ["ano"] )?$_GET["ano"] :1900;
-    echo "Voce nasceu em $a";
+    echo "Voce nasceu em $a e tera $i anos.<br/>";
+    if {$i >= 18 }  {
+          $v = "ja pode votar";
+          $d = "ja pode dirigir";
+    }
+    else {
+       $v = "nao pode votar";
+       $d = "nao pode dirigir";
+    }
+    echo "Com essa idade voce $v e tambem $d";
+    ?>
     
 </body>
 </html>
