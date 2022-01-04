bd2aab90607d60a32a17e47f2ab2057f7a96cc97
karinedapaz@gmail.com <samiradaguerra@gmail.com>
2022-01-04 09:06:04 -0300

    aula09 exercicio 2

--- aula09/exercicio02.php
+++ aula09/exercicio02.php
@@ -11,15 +11,17 @@
     $a = isset($_GET ["ano"] )?$_GET["ano"] :1900;
     $i = date ("Y") - $a;
     echo "Voce nasceu em $a e tera $i anos. <br/>";
-    if ($i >= 18)  {
-          $v = "ja pode votar";
-          $d = "ja pode dirigir";
+    if ($i >= 16)  {
+        $tipoVoto = "nao vota";
     }
-    else {
-       $v = "nao pode votar";
-       $d = "nao pode dirigir";
+    else  {
+        if  ($i  >=  16 &&  < 18)  {
+            $tipoVoto = "voto opcional";
+        }
+        else  {
+            $tipoVoto = "voto obrigatorio";
+        }
     }
-    echo "Com essa idade voce $v e tambem $d";
     ?>
     
 </body>
