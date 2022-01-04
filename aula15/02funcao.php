7904a476a8973e1d193e4537af1f33151d4b0be9
karinedapaz <karinedapaz@gmail.com>
2022-01-04 19:42:32 -0300

    rotinas em php

---
+++ aula15/02funcao.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <meta charset="UTF-8">
+    <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+         include "funcoes.php";
+         echo "<h1>Testando novas funcoes</h1>";
+         ola ();
+         mostraValor (4) ;
+         echo "<h2>Finalizando Programa...</h2>";
+    ?>
+</div>
+</body>
+</html>
