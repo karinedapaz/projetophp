3be11c46e2a79360b041c6e754aabf165120e47f
karinedapaz <karinedapaz@gmail.com>
2022-01-05 02:26:08 -0300

    vetores e matrizes

---
+++ aula17/modelo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <meta charset="UTF-8">
+    <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        $nome = "Gustavo Guanabara";
+        $nome2 = strtolower($nome);
+        echo "Seu nome e $nome2"; 
+    ?>
+</div>
+</body>
+</html>
